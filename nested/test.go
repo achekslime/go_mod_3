2ea74207f34451402ea6bebc14335447e3ff5fec
Asatiani Timur <impsface@yandex.ru>
2023-04-21 04:02:36 +0300

    logrus

--- nested/test.go
+++ nested/test.go
@@ -1,5 +1,10 @@
 package nested
 
+import (
+	"github.com/sirupsen/logrus"
+)
+
 func GetNestedMessage() string {
+	logrus.Info("lol")
 	return "nested message go_mod_3"
 }
--- test.go
+++ test.go
@@ -1,5 +1,8 @@
 package go_mod_3
 
+import "github.com/sirupsen/logrus"
+
 func GetMessage() string {
+	logrus.Info("lol")
 	return "go_mod_3"
 }
